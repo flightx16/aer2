6819c269451b66e0c6b2efd139fee0c8ed55f47d
yilun.wu <david.yilun.wu@gmail.com>
2015-02-17 12:55:16 -0500

    Tuesday Commit Up to drop warning zone

--- Game_Strategy/Game_serial_whole_status.ino
+++ Game_Strategy/Game_serial_whole_status.ino
@@ -5,10 +5,17 @@
 #define Tabuki 2
 #define SS 3; //Sensitivity Spot
 #define FS 4; //Forbidden Spot
+#define WS 5; //Warning Spot
+#define TRUE 1;
 typedef struct {
 	int Player; // 1-Lala, 2-Tabuki, 0-None
 	int Sensitivity;//Sensitivity zone
 	int Forbidden;//Forbidden zone
+	int Warning;//Warning zone
+	int V4;//the spot is already in a vertical connecting four sequence
+	int H4;//the spot is already in a horizontal connecting four sequence
+	int D41;//the spot is already in a diagonal type 1 connecting four sequence
+	int D42;//the spot is already in a diagonal type 2 connecting four sequence
 } Piece;
 
 Piece Board[NUM_COL][NUM_ROW];
@@ -21,6 +28,11 @@ void initialize (){
 			Board[i][j].Player = NONE;
 			Board[i][j].Sensitivity = NONE;
 			Board[i][j].Forbidden = NONE;
+			Board[i][j].Warning = NONE;
+			Board[i][j].V4 = NONE;
+			Board[i][j].H4 = NONE;
+			Board[i][j].D41 = NONE;
+			Board[i][j].D42 = NONE;
 		}
 	}
 }
@@ -31,6 +43,11 @@ void reset(){
 			Board[i][j].Player = NONE;
 			Board[i][j].Sensitivity = NONE;
 			Board[i][j].Forbidden = NONE;
+			Board[i][j].Warning = NONE;
+			Board[i][j].V4 = NONE;
+			Board[i][j].H4 = NONE;
+			Board[i][j].D41 = NONE;
+			Board[i][j].D42 = NONE;
 		}
 	}
 }
@@ -120,7 +137,9 @@ void loop(){
 
 int strategy(){
 	refresh();
+	identify_connecting_four();
 	identify_sensitivity_forbidden();
+	identify_warning();
 	int emergency_block_col_num;
 	emergency_block_col_num = block_vertical_three();
 	if (emergency_block_col_num >= 0 && emergency_block_col_num < NUM_COL) return emergency_block_col_num;
@@ -128,6 +147,17 @@ int strategy(){
 	if (emergency_block_col_num >= 0 && emergency_block_col_num < NUM_COL) return emergency_block_col_num;
 	emergency_block_col_num = block_horizontal_three();
 	if (emergency_block_col_num >= 0 && emergency_block_col_num < NUM_COL) return emergency_block_col_num;
+	emergency_block_col_num = trap_1();
+	if (emergency_block_col_num >= 0 && emergency_block_col_num < NUM_COL) return emergency_block_col_num;
+	emergency_block_col_num = trap_2();
+	if (emergency_block_col_num >= 0 && emergency_block_col_num < NUM_COL) return emergency_block_col_num;
+	if (Board[3][4].Player == NONE) return 3;
+	emergency_block_col_num = drop_warning();
+	if (emergency_block_col_num >= 0 && emergency_block_col_num < NUM_COL) return emergency_block_col_num;
+	
+
+	
+
 	return 1;
 }
 
@@ -136,10 +166,130 @@ void refresh(){
 		for (int j = 0; j < NUM_ROW; j++){
 			Board[i][j].Sensitivity = NONE;
 			Board[i][j].Forbidden = NONE;
+			Board[i][j].Warning = NONE;
+		}
+	}
+}
+/***************************************************************************************************************************************************/
+void identify_connecting_four(){
+	identify_connecting_four_hor();
+	identify_connecting_four_ver();
+	identify_connecting_four_dia1();
+	identify_connecting_four_dia2();
+}
+
+void identify_connecting_four_hor(){
+	for (int i = 0; i < 4; i ++){
+		for (int j = 0; j < NUM_ROW; j ++){
+			if (Board[i][j].Player == Tabuki && Board[i+1][j].Player == Tabuki && Board[i+2][j].Player == Tabuki && Board[i+3][j].Player == Tabuki){
+				Board[i][j].H4 = 1; Board[i+1][j].H4 = 1; Board[i+2][j].H4 = 1; Board[i+3][j].H4 = 1;
+			}
 		}
 	}
 }
 
+void identify_connecting_four_ver(){
+	for (int i = 0; i < NUM_COL; i ++){
+		for (int j = 0; j <3; j ++){
+			if (Board[i][j].Player == Tabuki && Board[i][j+1].Player == Tabuki && Board[i][j+2].Player == Tabuki && Board[i][j+3].Player == Tabuki){
+				Board[i][j].V4 = 1; Board[i][j+1].V4 = 1; Board[i][j+2].V4 = 1; Board[i][j+3].V4 = 1;
+			}
+		}
+	}
+
+}
+
+void identify_connecting_four_dia1(){
+	for (int i = 0; i < 4; i ++){
+		for (int j = 0; j < 3; j ++){
+			if (Board[i][j].Player == Tabuki && Board[i+1][j+1].Player == Tabuki && Board[i+2][j+2].Player == Tabuki && Board[i+3][j+3].Player == Tabuki){
+				Board[i][j].D41 == 1 ; Board[i+1][j+1].D41 == 1 ; Board[i+2][j+2].D41 == 1 ; Board[i+3][j+3].D41 == 1;
+			}
+		}
+	}
+
+}
+
+void identify_connecting_four_dia2(){
+	for (int i = 3; i < NUM_COL; i ++){
+		for (int j = 0; j < 3; j ++){
+			if (Board[i][j].Player == Tabuki && Board[i-1][j+1].Player == Tabuki && Board[i-2][j+2].Player == Tabuki && Board[i-3][j+3].Player == Tabuki){
+				Board[i][j].D42 == 1 ; Board[i-1][j+1].D42 == 1 ; Board[i-2][j+2].D42 == 1 ; Board[i-3][j+3].D42 == 1;
+			}
+		}
+	}
+}
+/************************************/
+void identify_warning(){
+	identify_warning_hor();
+	identify_warning_dia();
+}
+/***************************************************************************************************************************************************/
+void identify_warning_hor(){
+	for (int i = 0; i < 4; i ++){
+		for (int j = 1; j < NUM_ROW; j++){
+			markwarning_hor(i,j);
+		}
+	}
+}
+//mark forbidden zone for a connecting four sequence beginning at row_num, col_num
+void markwarning_hor(int col_num, int row_num){
+	int Tabuki_spot_num = 0;
+	for(int i=col_num; i<col_num + 4; i++){
+	    if (Board[i][row_num].Player == Lala || Board[i][row_num].H4 == 1) return ;
+	    else if (Board[i][row_num].Player == Tabuki) Tabuki_spot_num++;
+	}
+	if (Tabuki_spot_num >=2 ){
+		for(int i=col_num; i<col_num + 4; i++){
+			if (Board[i][row_num].Player == NONE ) { Board[i][row_num].Warning = WS;}
+		}
+	}	
+}
+/***************************************************************************************************************************************************/
+void identify_warning_dia(){
+	for(int i = 0; i < 4; i ++){
+		for(int j = 0; j <3 ;j ++){
+			markwarning_dia1(i,j);
+		}
+	}
+	for(int i = 3; i < NUM_COL; i++){
+		for(int j = 0; j <3 ;j ++){
+			markwarning_dia2(i,j);
+		}	    
+	}
+}
+
+void markwarning_dia1(int col_num, int row_num){
+	char Tabuki_spot_num = 0;
+	for(int i=0; i< 4; i++){
+	    if (Board[col_num + i][row_num + i].Player == Lala|| Board[col_num + i][row_num + i].D41 == 1) return ;
+	    else if (Board[col_num + i][row_num + i].Player == Tabuki) Tabuki_spot_num++;
+	}
+	if (Tabuki_spot_num >=2){
+		for(int i=0; i< 4; i++){
+			if (Board[col_num + i][row_num + i].Player == NONE) {
+				Board[col_num + i][row_num + i].Warning = WS;
+			}
+		}
+	}	
+}
+
+void markwarning_dia2(int col_num, int row_num){
+	char Tabuki_spot_num = 0;
+	for(int i=0; i< 4; i++){
+	    if (Board[col_num - i][row_num + i].Player == Lala || Board[col_num - i][row_num + i].D42 == 1) return ;
+	    else if (Board[col_num - i][row_num + i].Player == Tabuki) Tabuki_spot_num++;
+	}
+	if (Tabuki_spot_num >=2){
+		for(int i=0; i< 4; i++){
+			if (Board[col_num - i][row_num + i].Player == NONE ) {
+				Board[col_num - i][row_num + i].Warning = WS;
+			}
+		}
+	}	
+}
+/*****************HHHHHHHHHHHHHHH************/
+/***************************************************************************************************************************************************/
 void identify_sensitivity_forbidden(){
 	identify_forbidden_hor();
 	identify_forbidden_dia();
@@ -156,12 +306,12 @@ void identify_forbidden_hor(){
 void markforbidden_hor(int col_num, int row_num){
 	int Tabuki_spot_num = 0;
 	for(int i=col_num; i<col_num + 4; i++){
-	    if (Board[i][row_num].Player == Lala) return ;
+	    if (Board[i][row_num].Player == Lala || Board[i][row_num].H4 == 1) return ;
 	    else if (Board[i][row_num].Player == Tabuki) Tabuki_spot_num++;
 	}
 	if (Tabuki_spot_num >=2 ){
 		for(int i=col_num; i<col_num + 4; i++){
-			if (Board[i][row_num].Player == NONE && Board[i][row_num - 1].Player == NONE) Board[i][row_num - 1].Forbidden = FS;
+			if (Board[i][row_num].Player == NONE && Board[i][row_num - 1].Player == NONE) {Board[i][row_num - 1].Forbidden = FS;}
 		}
 	}	
 }
@@ -182,12 +332,14 @@ void identify_forbidden_dia(){
 void markforbidden_dia1(int col_num, int row_num){
 	char Tabuki_spot_num = 0;
 	for(int i=0; i< 4; i++){
-	    if (Board[col_num + i][row_num + i].Player == Lala) return ;
+	    if (Board[col_num + i][row_num + i].Player == Lala|| Board[col_num + i][row_num + i].D41 == 1) return ;
 	    else if (Board[col_num + i][row_num + i].Player == Tabuki) Tabuki_spot_num++;
 	}
 	if (Tabuki_spot_num >=2){
 		for(int i=0; i< 4; i++){
-			if (Board[col_num + i][row_num + i].Player == NONE && (row_num + i - 1) >= 0 && Board[col_num+ i][row_num + i - 1].Player == NONE) Board[col_num + i][row_num + i - 1].Forbidden = FS;
+			if (Board[col_num + i][row_num + i].Player == NONE && (row_num + i - 1) >= 0 && Board[col_num+ i][row_num + i - 1].Player == NONE) {
+				Board[col_num + i][row_num + i - 1].Forbidden = FS;
+			}
 		}
 	}	
 }
@@ -195,12 +347,14 @@ void markforbidden_dia1(int col_num, int row_num){
 void markforbidden_dia2(int col_num, int row_num){
 	char Tabuki_spot_num = 0;
 	for(int i=0; i< 4; i++){
-	    if (Board[col_num - i][row_num + i].Player == Lala) return ;
+	    if (Board[col_num - i][row_num + i].Player == Lala || Board[col_num - i][row_num + i].D42 == 1) return ;
 	    else if (Board[col_num - i][row_num + i].Player == Tabuki) Tabuki_spot_num++;
 	}
 	if (Tabuki_spot_num >=2){
 		for(int i=0; i< 4; i++){
-			if (Board[col_num - i][row_num + i].Player == NONE && (row_num + i - 1) >= 0 && Board[col_num - i][row_num + i - 1].Player == NONE) Board[col_num - i][row_num + i - 1].Forbidden = FS;
+			if (Board[col_num - i][row_num + i].Player == NONE && (row_num + i - 1) >= 0 && Board[col_num - i][row_num + i - 1].Player == NONE) {
+				Board[col_num - i][row_num + i - 1].Forbidden = FS;
+			}
 		}
 	}	
 }
@@ -211,9 +365,10 @@ int block_vertical_three(){
 	for (int i = 0; i < NUM_COL; i ++){
 		if (Board[i][2].Player == Lala || Board[i][3].Player == Lala) continue;
 		for (int j = 0; j < 3; j ++){
-			if (Board[i][j].Player == Tabuki && Board[i][j+1].Player == Tabuki && Board[i][j+2].Player == Tabuki && Board[i][j+3].Player == NONE) {num_block++; blockv3_col_num = i;}
+			if (Board[i][j].Player == Tabuki && Board[i][j+1].Player == Tabuki && Board[i][j+2].Player == Tabuki && Board[i][j+3].Player == NONE 
+				&& Board[i][j].V4 == 0 && Board[i][j+1].V4 == 0 && Board[i][j+2].V4 == 0 && Board[i][j+3].V4 == 0) {num_block++; blockv3_col_num = i;}
 		}
-		}
+	}
 	//if (num_block > 1) call selection function and then return from selection function
 	//else if (num_block == 1) return block_col_num;
 	if (num_block > 1) return blockv3_col_num;
@@ -247,7 +402,7 @@ int block_diagonal_three(){
 int check_diagonal_three1(int col_num, int row_num){
 	char Tabuki_spot_num = 0;
 	for (int i = 0; i < 4; i ++){
-		if (Board[col_num + i][row_num + i].Player == Lala) return 4;//Connecting four not detected for the specific sequence
+		if (Board[col_num + i][row_num + i].Player == Lala || Board[col_num + i][row_num + i].D41 == 1) return 4;//Connecting four not detected for the specific sequence
 		else if (Board[col_num + i][row_num + i].Player == Tabuki) Tabuki_spot_num++;
 	}
 	if (Tabuki_spot_num == 3){
@@ -261,7 +416,7 @@ int check_diagonal_three1(int col_num, int row_num){
 int check_diagonal_three2(int col_num, int row_num){
 	char Tabuki_spot_num = 0;
 	for (int i = 0; i < 4; i ++){
-		if (Board[col_num - i][row_num + i].Player == Lala) return 4;//Connecting four not detected for the specific sequence
+		if (Board[col_num - i][row_num + i].Player == Lala || Board[col_num - i][row_num + i].D42 == 1) return 4;//Connecting four not detected for the specific sequence
 		else if (Board[col_num - i][row_num + i].Player == Tabuki) Tabuki_spot_num++;
 	}
 	if (Tabuki_spot_num == 3){
@@ -280,6 +435,7 @@ int block_horizontal_three(){
 		for (int i = 0; i < 4; i ++){
 			char Tabuki_spot_num = 0;
 			for (int k = 0; k < 4; k ++){
+				if (Board[i+k][j].H4 == 1) break;
 				if (Board[i+k][j].Player == Tabuki) Tabuki_spot_num++;
 			}
 			if (Tabuki_spot_num == 3){
@@ -289,7 +445,35 @@ int block_horizontal_three(){
 			}
 		}
 	}
-	if (num_block > 1) return blockh3_col_num;
+	if (num_block > 1) return blockh3_col_num;//cal  selection
 	else if (num_block == 1) return blockh3_col_num;
 	return 8;
+}
+
+int trap_1(){
+	for (int j = 0; j < 1; j ++){
+		if (Board[0][j].Player == NONE && Board[1][j].Player == NONE && Board[2][j].Player == Tabuki && Board[3][j].Player == Tabuki &&Board[4][j].Player == NONE &&Board[5][j].Player == NONE ) return 4;
+		if (Board[1][j].Player == NONE && Board[2][j].Player == NONE && Board[3][j].Player == Tabuki && Board[4][j].Player == Tabuki &&Board[5][j].Player == NONE &&Board[6][j].Player == NONE ) return 2;
+		return 8;
+	}
+}
+
+int trap_2(){
+	if (Board[1][0].Player == NONE && Board[2][0].Player == Tabuki && Board[3][0].Player == Tabuki && Board[4][0].Player == NONE && Board[5][0].Player == NONE) return 4;
+	if (Board[1][0].Player == NONE && Board[2][0].Player == NONE && Board[3][0].Player == Tabuki && Board[4][0].Player == Tabuki && Board[5][0].Player == NONE) return 2;
+	return 8;
+}
+
+int drop_warning(){
+	char drop_warning_num = 0;
+	int place_col_num;
+	for (int i = 0; i < NUM_COL; i ++){
+		if (check_top_row(i) < NUM_ROW && Board[i][check_top_row(i)].Warning == 5 ){
+			drop_warning_num++; 
+			place_col_num = i;
+		}
+	}
+	if (drop_warning_num == 1) return place_col_num;
+	if (drop_warning_num > 1) return place_col_num; //use selection to decide
+	if (drop_warning_num == 0) return 8;
 }
